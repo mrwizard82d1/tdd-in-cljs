f15f56ad9172919984cfd142da3689874191a381
Larry Jones <larryj@blinds.com>
2017-11-12 17:21:34 -0600

    (BROKE) Add figwheel (and cljsbuild)
    This commit is broke because the generated project skeleton generated
    .clj files and not .cljs files.

--- project.clj
+++ project.clj
@@ -4,4 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.946"]])
+                 [org.clojure/clojurescript "1.9.946"]]
+  :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
+            [lein-figwheel "0.5.6"]]
+
+  :cljsbuild {:builds [{:id "dev" ; development configuration
+                        :source-paths ["src"] ; paths to monitor for changes
+                        :figwheel true ; enable Figwheel
+                        :compiler {:main tdd-in-cljs.core ; the main namespace
+                                   :asset-path "cljs/out" ; relative location of load-dependent files
+                                   :output-to "resources/public/cljs/main.js" ; where main file will be built
+                                   :output-dir "resources/public/cljs/out" ; directory for temporary files
+                                   :source-map-timestamp true}}]}) ; Source maps - hurray!
+
+
