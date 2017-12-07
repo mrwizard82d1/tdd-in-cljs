5724e4ff8efbddad86eedd1ae8c4d3531a4c1568
Larry Jones <larryj@blinds.com>
2017-12-06 22:11:22 -0600

    Move cljsbuild test command to top of hash

--- project.clj
+++ project.clj
@@ -12,27 +12,30 @@
             [lein-figwheel "0.5.14"]]
   :clean-targets ^{:protect false} [:target-path "out" "resources/public/cljs"]
   :figwheel {:css-dirs ["resources/public/css"]} ;; automated reload of CSS files
-  :cljsbuild {:builds [{:id "dev" ; development configuration
-                        :source-paths ["src"] ; paths to monitor for changes
-                        :figwheel true ; enable Figwheel
-                        :compiler {:main tdd-in-cljs.core ; the main namespace
-                                   :asset-path "cljs/out" ; relative location of load-dependent files
-                                   :output-to "resources/public/cljs/main.js" ; where main file will be built
-                                   :output-dir "resources/public/cljs/out" ; directory for temporary files
-                                   :source-map-timestamp true}} ; Source maps - hurray!
-                       {:id "test"
-                        :source-paths ["src" "test"]
-                        :compiler {:main runners.doo
-                                   :optimizations :none
-                                   :output-to "resources/public/cljs/tests/all-tests.js"}}
-                       {:id "devcards-test"
-                        :source-paths ["src" "test"]
-                        :figwheel {:devcards true}
-                        :compiler {:main runners.browser
-                                   :optimizations :none
-                                   :asset-path "cljs/tests/out"
-                                   :output-dir "resources/public/cljs/tests/out"
-                                   :output-to "resources/public/cljs/tests/all-tests.js"
-                                   :source-map-timestamp true}}]
-              :test-commands {"test" ["lein" "doo" "phantom" "test" "once"]}})
+  :cljsbuild
+  {
+   :test-commands {"test" ["lein" "doo" "phantom" "test" "once"]}
+   :builds [{:id "dev" ; development configuration
+             :source-paths ["src"] ; paths to monitor for changes
+             :figwheel true ; enable Figwheel
+             :compiler {:main tdd-in-cljs.core ; the main namespace
+                        :asset-path "cljs/out" ; relative location of load-dependent files
+                        :output-to "resources/public/cljs/main.js" ; where main file will be built
+                        :output-dir "resources/public/cljs/out" ; directory for temporary files
+                        :source-map-timestamp true}} ; Source maps - hurray!
+            {:id "test"
+             :source-paths ["src" "test"]
+             :compiler {:main runners.doo
+                        :optimizations :none
+                        :output-to "resources/public/cljs/tests/all-tests.js"}}
+            {:id "devcards-test"
+             :source-paths ["src" "test"]
+             :figwheel {:devcards true}
+             :compiler {:main runners.browser
+                        :optimizations :none
+                        :asset-path "cljs/tests/out"
+                        :output-dir "resources/public/cljs/tests/out"
+                        :output-to "resources/public/cljs/tests/all-tests.js"
+                        :source-map-timestamp true}}]})
+
 
