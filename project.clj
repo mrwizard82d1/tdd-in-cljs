549df5f3ad33eb55eaf2f08731e733980dcc37cd
Larry Jones <larryj@blinds.com>
2017-12-09 11:12:55 -0600

    Successfully use figwheel (almost)
    Everything works - except... When I execute `(js/alert "Hello")` in the
    Figwheel REPL, I see the dialog and then it immediately dissappers. I
    see no error messages (any of the places I looked).

--- project.clj
+++ project.clj
@@ -1,9 +1,19 @@
 (defproject tdd-in-cljs "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mrwizard82d1/tdd-in-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot tdd-in-cljs.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.946"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.14"]]
+  :cljsbuild 
+  {:builds [{:id "dev" ; development configuration
+             :source-paths ["src"] ; paths to monitor for build
+             :figwheel true ; Enable Figwheel
+             :compiler {:main tdd-in-cljs.core ; your main namespace
+                        :asset-path "cljs/out" ; relative path for load-dependent files 
+                        :output-to "resources/public/cljs/main.js" ; location of main build file
+                        :output-dir "resources/public/cljs/out" ; directory for temporary files
+                        :source-map-timestamp true} ; source maps - hurray!
+             }]})
---
+++ src/tdd_in_cljs/core.cljs
@@ -0,0 +1,10 @@
+(ns tdd-in-cljs.core)
+
+(defn main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (enable-console-print!)
+  (prn "Hello, 8th Light ClojureScript World!"))
+
+(main)
+
