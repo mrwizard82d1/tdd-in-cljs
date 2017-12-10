2630a6eae4b88cbfffd31c706ba1f418adbb8ea7
Larry Jones <larryj@blinds.com>
2017-12-09 20:20:25 -0600

    Asynchronous tests throwing an exception

--- test/tdd_in_cljs/core_test.cljs
+++ test/tdd_in_cljs/core_test.cljs
@@ -1,7 +1,14 @@
 (ns tdd-in-cljs.core-test
-  (:require [cljs.test :refer-macros [is testing]]
+  (:require [cljs.test :refer-macros [is testing async]]
             [devcards.core :refer-macros [deftest]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
+
+(deftest b-test
+         (testing "FIXME asynchronously, I fail."
+           (async done
+             (js/setTimeout (fn []
+                              (throw (js/Error. "Oops!")))
+                            100))))
