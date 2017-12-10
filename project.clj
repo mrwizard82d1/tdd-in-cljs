9716564fdea7fd3502976876c692400b242cc06f
Larry Jones <larryj@blinds.com>
2017-12-09 18:53:01 -0600

    Set up browser testing using devcards

--- project.clj
+++ project.clj
@@ -4,13 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[lein-doo "0.1.8"]
+                 [devcards "0.2.4"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.946"]]
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [lein-doo "0.1.8"]
             [lein-figwheel "0.5.14"]]
   :clean-targets ^{:protect false} [:target-path "out" "resources/public/cljs"]
-  :figwheel {:css-dirs "resources/public/css"}
+  :figwheel {:css-dirs ["resources/public/css"]}
   :cljsbuild 
   {:test-commands {"test" ["lein" "doo" "phantom" "test" "once"]}
    :builds [{:id "dev" ; development configuration
@@ -25,5 +26,14 @@
              :source-paths ["src" "test"]
              :compiler {:main runners.doo
                         :optimizations :none
-                        :output-to "resources/public/cljs/tests/all-tests.js"}}]})
+                        :output-to "resources/public/cljs/tests/all-tests.js"}}
+            {:id "devcards-test"
+             :source-paths ["src" "test"]
+             :figwheel {:devcards true}
+             :compiler {:main runners.browser
+                        :optimizations :none
+                        :asset-path "cljs/tests/out"
+                        :output-dir "resources/public/cljs/tests/out"
+                        :output-to "resources/public/cljs/tests/all-tests.js"
+                        :source-map-timestamp true}}]})
 
