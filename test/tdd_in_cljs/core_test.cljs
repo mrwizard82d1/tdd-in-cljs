5908330a3ddd980315a8d2e22691ccfbf900488b
Larry Jones <larryj@blinds.com>
2017-12-09 19:25:21 -0600

    Passing (synchronous) test using Devcards

---
+++ test/tdd_in_cljs/core_test.cljs
@@ -0,0 +1,7 @@
+(ns tdd-in-cljs.core-test
+  (:require [cljs.test :refer-macros [is testing]]
+            [devcards.core :refer-macros [deftest]]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
